75da74c2e099cc68cb5ea0cafc40b7c367685110
yazankittaneh <Licardo7>
2015-06-19 09:30:18 -0500

    added email controller

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,24 @@
+class EmailsController < ApplicationController
+
+  def new
+    @email = Email.new
+  end
+
+  def create
+    #@email = Email.new(params.require(:email).permit(:address))
+    #if @email.save
+    #  redirect_to action: 'thank_you'
+    #end
+  end
+
+  def show
+  end
+
+  def thank_you
+  end
+
+
+#    if params[:email] == [/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i]
+#    redirect_to :controller=>'tips', :action => 'show', :id => @tip.permalink
+
+end
