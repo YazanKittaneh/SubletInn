218fc5035ddf4be121558f1e23abd300469b5ac5
yazankittaneh <kittaneh17@grinnell.edu>
2015-07-18 01:59:48 -0500

    change routes after heroku fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :francis, :welcome, :emails
 
   # You can have the root of your site routed with "root"
-   root 'francis#index'
+   root 'welcome#index'
 
 
   # Example of regular route:
