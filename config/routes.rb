0990606125fecc5859209182d4fd3846cda8911e
wuruthie <wuruth17@grinnell.edu>
2015-07-21 19:47:37 -0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :francis, :welcome, :emails
+  resources :francis, :subletinn, :emails
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'subletinn#index'
 
 
   # Example of regular route:
