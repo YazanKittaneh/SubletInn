61dc13fe237ea2b06053e135479dceb303682e90
wuruthie <wuruth17@grinnell.edu>
2015-07-29 13:20:00 -0500

    added footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :francis, :subletinn, :emails
 
   # You can have the root of your site routed with "root"
-   root 'subletinn#main'
+   root 'francis#index'
 
 
   # Example of regular route:
