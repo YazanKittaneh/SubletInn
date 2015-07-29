dd776e4b2da6c17eee2c8ba7a5d28d699ef064c5
yazankittaneh <kittaneh17@grinnell.edu>
2015-07-29 14:02:06 -0500

    created payment tokens

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,20 +2,26 @@ class PaymentsController < ActionController::Base
 
 def new
   @client_token = Braintree::ClientToken.generate
+  @amount
 end
 
 
 def create
   nonce = params[:payment_method_nonce]
   render action: :new and return unless nonce
+
   result = Braintree::Transaction.sale(
-    amount: "10.00",
+    amount: params[:plan],
     payment_method_nonce: nonce
   )
 
   flash[:success] = "Sale successful. Head to Sizzler" if result.success?
-  flash[:notice] = "Something is amiss. #{result.transaction.processor_response_text}" unless result.success?
+  flash[:notice] = "Something is amiss. #{result.subscription.errors}" unless result.success?
   redirect_to action: :new
+end
+
+def amount
+  @amount = params[:amount]
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   post "/checkout", to: 'payments#create'
 
+  get '/show', to: 'payments#new'
+
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
