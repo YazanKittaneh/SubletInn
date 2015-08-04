f1abdb83b6e23e9c14875bc4a9cfc727d557fe66
wuruthie <wuruth17@grinnell.edu>
2015-08-04 15:49:25 -0500

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :francis, :subletinn, :emails
 
   # You can have the root of your site routed with "root"
-   root 'francis#index'
+   root 'subletinn#main'
 
 
   # Example of regular route:
